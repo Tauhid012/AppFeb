42599ae86b664577c27f20181be396c028a2df08
parth <parthdarak11@gmail.com>
2022-03-17 21:10:01 +0530

    jsp added

--- src/main/java/com/codeplanet/Test.java
+++ src/main/java/com/codeplanet/Test.java
@@ -6,6 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.SplittableRandom;
 
@@ -30,7 +34,7 @@ public class Test {
 	String email = req.getParameter("email");
 	String psw = req.getParameter("psw");
 	System.out.print("signUpMethod is working" + email + " password is " + psw);
-	return "First";
+	return "home";
   }
   
   @PostMapping("/signUp")
@@ -70,51 +74,34 @@ public class Test {
 	p.put("mail.smtp.port", "465");
 	p.put("mail.smtp.ssl.enable", "true");
 	p.put("mail.smtp.auth", "true");
-	
-	
 	MailAuthenticator m = new MailAuthenticator("testcodeplanet@gmail.com", "9806584240");
 	
 	Session session = Session.getInstance(p, m);
 	session.setDebug(true);
-	
 	MimeMessage msg = new MimeMessage(session);
-	
 	try {
 		msg.setFrom("testcodeplanet@gmail.com");
-		
 		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
-		
 		msg.setSubject(subject);
-		
 		msg.setText(body);
-		
 		Transport.send(msg);
-		
 	} catch (MessagingException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
   }
 
-public String generateOtp(int size) {
+  public String generateOtp(int size) {
 	StringBuilder sb = new StringBuilder();
 	SplittableRandom sp = new SplittableRandom();
-	 for (int i =0 ; i<size; i++) {
+    for (int i =0 ; i<size; i++) {
 		 int rn = sp.nextInt(0,9);
 		 sb.append(rn);
 	 }
 	return sb.toString();
   }
 
-@GetMapping("/login")
+  @GetMapping("/login")
   public String login(HttpServletRequest req) {
     return "login";
   }
@@ -134,7 +121,18 @@ public String generateOtp(int size) {
 			return "First";
 	   }
 	   if((rs.getString("PSW")).equals(psw)) {
-			req.setAttribute("test", "You are Successfully loggedin");
+			String query2 = "Select * from links where created_by='"+ email + "'";
+			ResultSet rs1 = stmt.executeQuery(query2);
+			List<Map<String,String>> l = new ArrayList<Map<String,String>>();
+			
+			while(rs1.next()) {
+			  Map<String,String> s = new HashMap<String,String>();
+			  s.put("longUrl", rs1.getString("long_link"));
+			  s.put("shortUrl", rs1.getString("short_link"));
+			  l.add(s);
+			}
+			req.setAttribute("list", l);
+		   return "AfterSignin";
 	   } else {
 		   req.setAttribute("test", "Your psw is not correct please check");
 	   }
@@ -170,4 +168,48 @@ public String otpVerification(HttpServletRequest req) throws SQLException, Class
 		}
 		return "First";
 		}
+
+  @GetMapping("/urlshortner")
+  public String UrlShortner(HttpServletRequest req) throws SQLException, ClassNotFoundException {
+	String link = req.getParameter("link");
+	String customurl = req.getParameter("customurl");
+	if (customurl != null && !customurl.isEmpty()) {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "admin123");
+		Statement stmt = con.createStatement();
+		String query1 = "Select * from links where short_link='"+ customurl + "'";
+		ResultSet rs = stmt.executeQuery(query1);
+		if(rs.next()) {
+	      req.setAttribute("error", "custom Url Already exist");
+		} else {
+			  String query2 = "insert into links(long_link,short_link) values(?,?)";
+			  PreparedStatement stmt1 = con.prepareStatement(query2);
+			  stmt1.setString(1,link);
+			  stmt1.setString(2, customurl);
+			  stmt1.executeUpdate();
+		      req.setAttribute("url", "Your new url is nano.cc/"+customurl);
+
+		}
+
+	} else {
+		
+	}
+	  return "home";
+  }
+  
+  @GetMapping("/{url}")
+  public String handleShortUrl(@PathVariable String url) throws SQLException, ClassNotFoundException {
+	Class.forName("com.mysql.jdbc.Driver");
+	Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "admin123");
+	PreparedStatement pstmt = con.prepareStatement("Select * from links where short_link =? ");
+	pstmt.setString(1, url);
+	ResultSet rs = pstmt.executeQuery();
+	if (rs.next()) {
+		String longLink = rs.getString("long_link");
+		return "redirect:"+longLink;
+		
+	} else {
+	  return "error";
+	}
+  }
 }
